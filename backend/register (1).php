bee52112a7914829bd643d9218fcc5594faadf41
Yudhajeet Bhattacharya <57074118+norserambler@users.noreply.github.com>
2020-09-19 20:38:54 +0530

    Add files via upload

---
+++ backend/register (1).php
@@ -0,0 +1,25 @@
+<?php
+$servername = "remotemysql.com";
+$database = "g6DbtocErH";
+$username = "g6DbtocErH";
+$password = "GJVUCglOMR";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+ 
+$number = $_POST['number'];
+$name = $_POST['name'];
+$password = $_POST['psw'];
+$sql = "INSERT INTO Hoemato (number, name, psw) VALUES ('$number', '$name', '$password')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
